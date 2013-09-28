ed3e895a49cdbe550ac112db596590f1e5e67123
Andrew Wei <lowershadow@foxmail.com>
2013-09-28 20:13:41 +0800

    add projectt

---
+++ SoLibs/SoLibs/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SoLibs
+//
+//  Created by Andrew on 13-9-28.
+//  Copyright (c) 2013年 Andrew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
